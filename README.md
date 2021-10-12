f6bc796d70c3889e80f5f21755979c240833dccb
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-10-12 22:21:15 +0300

    test: test 2 function parse-templete finished

---
+++ README.md
@@ -0,0 +1,8 @@
+# Lab: File IO and Exceptions
+
+Create a file called `madlib.py` at root of madlib_cli folder, which will contain all of the Python code that you will write relating to your Madlib game.
+
+
+1- read file function `(read_template)` which will take a file and then return the data in the file
+
+2-
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,14 +1,37 @@
+import re
 
+print("Welcome You are in the Madlib Game")
 
 def read_template(path):
-    file=open(path)
-    data=file.read()
+    """ function how read file from the url
+        and return what that file have 
+    """ 
+    a=open(path)
+    return a.read()
 
-    return data
+def parse_template(input):
+    '''
+    takes a text as input and return a tuple and a string without the words
+    in the tupple and uses regex to idenitify the required word 
+    '''
+    actual_stripped=''
+    actual_parts=[]
+
+    x=input.split(' ')
+    print(x)
+    reg=r"^{\w+}|\.$"
+    for i in x:
+        if re.match(reg,i)==None :
+            actual_stripped+=f"{i} "
+        else :
+            if i==x[-1]:
+                actual_stripped+='{}.'
+                actual_parts+=[i[1:-2]]
+            else:
+                actual_parts+=[i[1:-1]]
+                actual_stripped+='{} '
+
+    actual_parts=tuple(actual_parts)
+    return (actual_stripped,actual_parts)
 
 
-
-def parse_template():
-        pass
-def merge():
-        pass
